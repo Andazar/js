e2209a6c94fb735d7adde023f1d96e28c37494fb
Andazar <andazar89@gmail.com>
2021-02-24 15:17:01 +0500

    10

--- test.js
+++ test.js
@@ -5,15 +5,17 @@ const T1 = "Hello, JavaScript!!!";
 const T2 = "It is my test prog.";
 let T3;
 let T4;
+let N1 = 10;
+let N2 = 20;
 
 T3 = T2;
 T4 = T1;
 
-//[T1,T2] =[T2,T1];
+[N1,N2] =[N2,N1];
 
 console.log(T1, T2);//смотреть по F12
 console.log(T3, T4);//смотреть по F12
-//console.log('${T1} ${T2}');смотреть по F12
+console.log('${N1} ${N2}'); //смотреть по F12
 
 
 
